784af974356f9532c37fda2867db87f412c6ff14
Lukas <cc211027@fhstp.ac.at>
2024-04-04 15:19:40 +0200

    Implementation Start: GlobalState & BattleState

---
+++ Assets/Scripts/BattleStateManager.cs
@@ -0,0 +1,146 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class BattleStateManager
+{
+    public List<Card> DrawPile{ get; private set; }
+    public List<Card> Hand { get; private set; }
+    public List<Card> DiscardPile { get; private set; }
+    public List<Enemy> EnemiesInBattle { get;private set; }
+    public int CurrentPlayerHealth { get; set; }
+    public int CurrentActionPoints { get; set; }
+    public Stats PlayerStats { get; set; }
+    
+    public void Initialize(List<Card> deck, List<Enemy> enemies)
+    {
+        // Player stats, the deck and enemies of this battle
+        DrawPile = Shuffle(deck);
+        DiscardPile = new List<Card>();
+        Hand = new List<Card>();
+        CurrentPlayerHealth = GameStateManager.Instance.CurrentPlayerHealth;
+        CurrentActionPoints = GameStateManager.Instance.MaxActionPoints;
+        PlayerStats = new Stats();
+        EnemiesInBattle = enemies;
+        StartPlayerTurn();
+    }
+
+    /*
+     * Shuffle and return the passed deck
+     */
+    public List<Card> Shuffle(List<Card> deck)
+    {
+        Random r = new Random();
+        int n = deck.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = r.Next(n + 1);  
+            Card value = deck[k];  
+            deck[k] = deck[n];  
+            deck[n] = value;  
+        }
+        return deck;
+    }
+
+    /*
+     * Draw the upmost card from the draw pile and add it to the hand
+     */
+    public void DrawCard()
+    {
+        Hand.Add(DrawPile[DrawPile.Count-1]);
+        DrawPile.RemoveAt(DrawPile.Count-1);
+    }
+
+    /*
+     * Discard a specific card from the hand
+     */
+    public void DiscardCard(Card c)
+    {
+        DiscardPile.Add(c);
+        Hand.Remove(c);
+    }
+
+    /*
+     * Discard all cards in the hand
+     */
+    public void DiscardHand()
+    {
+        foreach (Card c in Hand)
+        {
+            DiscardCard(c);
+        }
+    }
+
+    /*
+     * Execute a turn for each enemy
+     */
+    public void EnemyTurn()
+    {
+        // Reduce insight of player
+        foreach (Enemy e in EnemiesInBattle)
+        {
+            // Apply and reduce burn/wound of enemy
+        
+            // Do action
+            
+            if (CurrentPlayerHealth <= 0)
+            {
+                EndBattle();
+            }
+            
+            // Show action for next turn
+            // Reduce insight  
+        }
+        StartPlayerTurn();
+    }
+
+    /*
+     * Start the turn for the player
+     */
+    public void StartPlayerTurn()
+    {
+        // Apply and reduce burn/wound of player
+        
+        // Draw until the player has 5 cards in the hand
+        while (Hand.Count < 5)
+        {
+            DrawCard();
+        }
+        CurrentActionPoints = GameStateManager.Instance.MaxActionPoints;
+    }
+
+    /*
+     * Battle has ended, either the player has won or died
+     */
+    public void EndBattle()
+    {
+        if (CurrentPlayerHealth <= 0)
+        {
+            // You lost
+            return;
+        }
+        
+        // You won
+        GameStateManager.Instance.CurrentPlayerHealth = CurrentPlayerHealth;
+        ShowReward();
+    }
+
+    /*
+     * Show three random cards to the player
+     */
+    public void ShowReward()
+    {
+        List<Card> rewards = new List<Card>();
+        Random r = new Random();
+        // Select the three random cards
+        while (rewards < 3)
+        {
+            int index = r.Next(Deck.AllAvailableCards.Count);
+            if (!rewards.Contains(Deck.AllAvailableCards[index]))
+            {
+                rewards.Add(Deck.AllAvailableCards[index]);
+            }
+        }
+        // Show them in a popup to the player
+    }
+}
