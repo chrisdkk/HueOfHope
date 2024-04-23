f7cb9aab5f76594d8f63273ab25ed0dedffb8fd8
Crysker <serkan5768@gmail.com>
2024-04-23 15:27:17 +0200

    animations, background new scenes

--- Assets/Scripts/Visuals/PlayerActionPointsUI.cs
+++ Assets/Scripts/Visuals/PlayerActionPointsUI.cs
@@ -1,14 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class PlayerActionPointsUI : MonoBehaviour
 {
-    // Update is called once per frame
+    public GameObject[] actionPoints;
+
     public void UpdateActionPoints()
     {
-        GetComponent<TextMeshProUGUI>().text = "AP: " + GameStateManager.Instance.BattleManager.PlayerScript.CurrentActionPoints +
-                                               " / " + GameStateManager.Instance.MaxActionPoints;
+        var textComponent = GetComponent<TextMeshProUGUI>();
+        if (textComponent != null)
+        {
+            textComponent.text = "AP: " + GameStateManager.Instance.BattleManager.PlayerScript.CurrentActionPoints +
+                                 " / " + GameStateManager.Instance.MaxActionPoints;
+        }
+        UpdateActionPointUI(); // Refresh the UI
+    }
+
+    void UpdateActionPointUI()
+    {
+        int currentActionPoints = GameStateManager.Instance.BattleManager.PlayerScript.CurrentActionPoints;
+
+        for (int i = 0; i < actionPoints.Length; i++)
+        {
+            SpriteRenderer spriteRenderer = actionPoints[i].GetComponent<SpriteRenderer>(); // Get SpriteRenderer component
+
+            if (spriteRenderer != null)
+            {
+                Color currentColor = spriteRenderer.color;
+
+                if (i < currentActionPoints)
+                {
+                    currentColor.a = 1.0f; // Full opacity
+                }
+                else
+                {
+                    currentColor.a = 0.2f; // Less opacity
+                }
+
+                spriteRenderer.color = currentColor; // Apply updated color
+
+                Debug.Log($"Adjusted opacity of action point {i} to {currentColor.a}");
+            }
+            else
+            {
+                Debug.LogWarning($"GameObject at index {i} does not have a SpriteRenderer component.");
+            }
+        }
     }
 }
