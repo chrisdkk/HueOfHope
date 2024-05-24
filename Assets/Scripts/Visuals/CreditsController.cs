8bf8c8e3db04da629484868be537b94f04ea40b1
ffuszthaler <flo.fuszthaler@gmail.com>
2024-05-24 12:42:16 +0200

    changed game over text

--- Assets/Scripts/Visuals/CreditsController.cs
+++ Assets/Scripts/Visuals/CreditsController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CreditsController : MonoBehaviour
 {
     private RectTransform scrollerTransform;
-    private float upperScrollLimit = 0;
+    private readonly float upperScrollLimit = 0;
 
     // Start is called before the first frame update
     void Start()
